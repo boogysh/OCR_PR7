250f164af74c71263cdf57a53edf7b1929424934
boogysh <bugavictor86@gmail.com>
2022-11-21 20:30:27 +0100

    add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home/home.jsx'
+import Header from './components/Header/header.jsx'
+import Footer from './components/Footer/footer.jsx'
+import About from './pages/About/about'
+import Error404 from './pages/Error/error404.jsx'
+import OneLodging  from './pages/OneLodging/oneLodging.jsx';
+
+function App(){
+  return(
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apropos" element={<About />} />
+        <Route path="/logements/:id" element={<OneLodging />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from './pages/Home/home.jsx'
-import Header from './components/Header/header.jsx'
-import Footer from './components/Footer/footer.jsx'
-import About from './pages/About/about'
-import Error404 from './pages/Error/error404.jsx'
-import OneLodging  from './pages/OneLodging/oneLodging.jsx';
+import App from './App';
 import './index.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apropos" element={<About />} />
-        <Route path="/logements/:id" element={<OneLodging />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <App/>
   </React.StrictMode>
 );
 
